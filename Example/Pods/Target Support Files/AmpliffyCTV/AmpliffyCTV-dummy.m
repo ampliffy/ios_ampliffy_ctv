c684e399c5b2dc3aa651c82aec54ba670108cb36
alfrediffy <alfred@ampliffy.com>
2024-08-29 14:23:51 +0200

    First Commit

---
+++ Example/Pods/Target Support Files/AmpliffyCTV/AmpliffyCTV-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AmpliffyCTV : NSObject
+@end
+@implementation PodsDummy_AmpliffyCTV
+@end
---
+++ Example/Pods/Target Support Files/Pods-AmpliffyCTV_Example/Pods-AmpliffyCTV_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AmpliffyCTV_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AmpliffyCTV_Example
+@end
